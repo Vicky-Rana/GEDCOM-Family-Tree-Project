02ab5950aed52ae23a18b9cbfe662a5bda52434f
Vicky Rana <Vicky Rana>
2017-03-05 23:36:22 -0500

    Code as per the Output Needed

---
+++ US04_Marriagebeforedivorce.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+#from collections import counter
+import re
+import sys
+import pymongo
+from pymongo import MongoClient
+from pprint import pprint
+from all_db_operations import *
+from print_data import *
+#DB connection
+connection = MongoClient('localhost', 27017)
+db = connection['GEDCOMDB']
+
+
+
+
+
+
+
+
+def check_marriagebeforedivorce():
+	
+	
+	#Search Family
+	fam1 = db.family.find({})
+	
+	results = [res for res in fam1] 
+	fam1.close()
+	
+	for res in results:
+		if "marriage" and "divorce" in res:
+			#print "Marriage_Date------",res["marriage"]
+			#print "Divorce_Date------",res["divorce"]
+			marr = res["marriage"]
+			divv = res["divorce"]
+		
+			#newdate1 = time.strptime(marr, "%d/%m/%Y")
+			#newdate2 = time.strptime(divv, "%d/%m/%Y")
+			if marr > divv:
+			
+				message = "Divorced"+str(divv)+", Before Marriage"+str(marr)+ ", therefore it is not valid."
+				save_invalid_family_for_print(res["FAMID"], "US04", message)
+
+
+
+	
+
+	
+
+			
---
+++ US06_Divorcebeforedeath.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timedelta
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+#from collections import counter
+import re
+import sys
+import pymongo
+from pymongo import MongoClient
+from pprint import pprint
+from all_db_operations import *
+from print_data import *
+#DB connection
+#connection = MongoClient('localhost', 27017)
+#db = connection['GEDCOMDB']
+
+
+
+def divorcebeforedeath():
+	
+	
+	#Search Family
+	
+	
+	
+	results = get_family() 
+	
+	for res in results:
+		if ("HUSBAND" or "WIFE") and "divorce" in res:
+			#print("___________"+res["HUSBAND"])
+			#print("___________"+res["WIFE"])
+			husband = res["HUSBAND"]
+			wife = res["WIFE"]
+			
+			indi = get_people()
+			for temp in indi:
+				if husband == temp["ID"] and "deathDate" in temp:
+					deathdth = temp["deathDate"]
+					divdateh = res["divorce"]
+					if divdateh > deathdth:
+						
+					
+
+			
+						message = "Divorced"+str(divdateh)+", After Husband"+str(husband)+ "Death on"+str(deathdth)+", therefore Divorce is not valid."
+						save_invalid_family_for_print(res["FAMID"], "US06", message)
+	
+				if wife == temp["ID"] and "deathDate" in temp:
+					deathdt = temp["deathDate"]
+					divdate = res["divorce"]
+					if divdate > deathdt:
+						
+						
+						message = "Divorced"+str(divdate)+", After Wife"+str(wife)+ "Death on"+str(deathdt)+", therefore Divorce is not valid."
+						save_invalid_family_for_print(res["FAMID"], "US06", message)
+		
+
+
+			
