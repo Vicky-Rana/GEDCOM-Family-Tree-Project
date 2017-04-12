b23edb6f50a948994dc9f95236ba49d9e98c0587
Abhilash Ugaonkar <abhilash410@gmail.com>
2017-04-11 22:59:31 -0400

    Added my user story 41 to the exe.py

--- exe.py
+++ exe.py
@@ -12,6 +12,7 @@ from US04_Marriagebeforedivorce import *
 from US05_marriageBeforeDeath import *
 from US06_Divorcebeforedeath import *
 from US07_lessThan150 import *
+from US08_birth_before_marriage_of_parents import *
 from US09_birth_before_death_of_parents import *
 from US10_Marriage_after_14 import *
 from US12_Parents_not_too_old import *
@@ -33,10 +34,11 @@ from US35_List_recent_births import *
 from US36_List_Recent_Deaths import *
 from US38_listUpcomingBirthdays import US38_listUpcomingBirthdays
 from US39_upcoming_anniversary import upcoming_anniversary
+from US41_Accept_Partial_Dates import US41_accpet_partial_dates
 from US42_Reject_illegitimate_dates import US42_rejectIllegitimateDates
 from print_data import *
 from src.gedParser import GEDCOMParser
-
+
 FILENAME = 'gedcom_files/myTree.ged'
 
 connection = MongoClient('localhost', 27017)
@@ -71,6 +73,8 @@ def printPretty(individual, families):
     divorcebeforedeath()
     #Call User story 07
     US07_lessThan150()
+    # call User story 08
+    US08_birth_before_marriage_of_parents()
     # Call User Story 09
     US09_birth_before_death_of_parents()
     #US10  User story 10
@@ -92,9 +96,6 @@ def printPretty(individual, families):
     unique_famids()
     # User Story 23
     unique_name_bdate()
-
-
-
     #Call User Story 25
     unique_first_famnames()
     # Call User story 27
@@ -107,23 +108,21 @@ def printPretty(individual, families):
     living_marriages()
     # Call user story 31
     more_than_30_unmarried()
-    
     #call user story 32
     US32_multiple_births()
     # Call User story 33
     US33_listOrphans()
-
     # Call User story 35
     US35_list_recent_births()
     #User 36
     recent_deaths()
-
-
     # Call User story 38
     US38_listUpcomingBirthdays()
     # Call User story 39
     upcoming_anniversary()
-
+    # Call User story 41
+    US41_accpet_partial_dates()
+    # Call User story 42
     US42_rejectIllegitimateDates()
 
     
